4bc3cf165c9ac55457f2ea5b62fbe5a05110c633
Kunlexi <adesinataofik5@gmail.com>
2023-03-04 23:32:22 +0100

    update

--- index.ts
+++ index.ts
@@ -1,10 +1,11 @@
 import express from "express";
 
+import { AdminRoute, VendorRoute } from "./routes";
+
 const app = express();
 
-app.use("/", (req, res) => {
-  return res.json("Hello from Food order Backend!!");
-});
+app.use("/admin", AdminRoute);
+app.use("/vendor", VendorRoute);
 
 app.listen(8000, () => {
   console.log("App is listening to the port 8000");
--- routes/AdminRoute.ts
+++ routes/AdminRoute.ts
@@ -1,5 +1,9 @@
-import express, { Request, Response, NextFunction } from "express";
+import express, { Request, Response, NextFunction, response } from "express";
 
 const router = express.Router();
+
+router.get("/", (req: Request, res: Response, next: NextFunction) => {
+  res.json({ message: "Hello from  Admin" });
+});
 
 export { router as AdminRoute };

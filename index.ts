7ed1221530d48245c33417fcf7790c8ff1859a1d
Kunlexi <adesinataofik5@gmail.com>
2023-03-05 21:50:00 +0100

    commit

--- index.ts
+++ index.ts
@@ -5,7 +5,7 @@ import { AdminRoute, VendorRoute } from "./routes";
 const app = express();
 
 app.use("/admin", AdminRoute);
-app.use("/vendor", VendorRoute);
+// app.use("/vendor", VendorRoute);
 
 app.listen(8000, () => {
   console.log("App is listening to the port 8000");
